227b8d46d0802f11fd271ca75124bf688ae387c4
Karl Andreas <kajohansen.1@gmail.com>
2014-09-16 09:38:51 +0200

    Removed debug controls again..

--- index.php
+++ index.php
@@ -437,15 +437,6 @@
 				<img id="dice_img" src="images/dice.svg" />
 			</div>
 			
-			<select id="select_dice_num">
-				<option value="6">Roll a Six</option>
-				<option value="5">Roll a Five</option>
-				<option value="4">Roll a Four</option>
-				<option value="3">Roll a Three</option>
-				<option value="2">Roll a Two</option>
-				<option value="1">Roll a One</option>
-			</select>
-			<button id="roll_num_btn">Roll</button>
 			<input id="sounds_box" type="checkbox">Background</input>
 			<input id="fx_box" type="checkbox">FX Sounds</input>
 		</div>
@@ -512,15 +503,5 @@
 	<script src='scripts/Dice.js' type='text/javascript'></script>
 	<script src='scripts/Player.js' type='text/javascript'></script>
 	<script src='scripts/LudoObj.js' type='text/javascript'></script>
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	
-	  ga('create', 'UA-54829981-1', 'auto');
-	  ga('send', 'pageview');
-	
-	</script>
 <!-- 	<script src='scripts/Ludo.js' type='text/javascript'></script> -->
 </html>
